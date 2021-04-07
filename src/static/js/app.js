15a9da2e539fd79e512d3a115d1c8bf06f8a1cdd
molgan <olga.matsuga@gmail.com>
2021-04-07 16:42:24 +0300

    Update app.js

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -96,7 +96,7 @@ function AddItemForm({ onNewItem }) {
                     value={newItem}
                     onChange={e => setNewItem(e.target.value)}
                     type="text"
-                    placeholder="Enter New Item"
+                    placeholder="Enter Item"
                     aria-describedby="basic-addon1"
                 />
                 <InputGroup.Append>
